61cd035bd28fc30b866415228d033c358026e4dd
Maria Kun <penelopa.alex@yandex.ru>
2025-02-16 15:03:34 +0300

    fix

--- bff-service/src/main/java/ru/netology/service/MyService.java
+++ bff-service/src/main/java/ru/netology/service/MyService.java
@@ -1,22 +1,38 @@
 package ru.netology.service;
 
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.netology.entity.GlobalUser;
 import ru.netology.entity.Order;
 import ru.netology.entity.User;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static io.restassured.RestAssured.given;
 
 @AllArgsConstructor
 @Service
 public class MyService {
-    private final UserService userService;
-    private final OrderService orderService;
 
     public GlobalUser getUser(int id) {
-        User user = userService.getUser(id);
-        List<Order> orderList = orderService.getOrders(id);
+        RequestSpecification spec = given()
+                .baseUri("http://localhost:8080/api");
+        
+        User user = given().spec(spec)
+                .contentType(ContentType.JSON)
+                .get("/users/" + id)
+                .then()
+                .extract().as(User.class);
+
+        List<Order> orderList = Arrays.stream(given().spec(spec)
+                .contentType(ContentType.JSON)
+                .get("/orders/by-user/" + id)
+                .then()
+                .extract().as(Order[].class)).toList();
+
         return new GlobalUser(user, orderList);
     }
 }
