8121b16cd2b27dea548bb805f166347a31936d4d
Maria Kun <penelopa.alex@yandex.ru>
2025-02-20 14:47:59 +0300

    fix

--- bff-service/src/main/java/ru/netology/service/MyService.java
+++ bff-service/src/main/java/ru/netology/service/MyService.java
@@ -1,38 +1,33 @@
 package ru.netology.service;
 
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 import ru.netology.entity.GlobalUser;
 import ru.netology.entity.Order;
 import ru.netology.entity.User;
 
 import java.util.Arrays;
-import java.util.List;
-
-import static io.restassured.RestAssured.given;
 
 @AllArgsConstructor
 @Service
 public class MyService {
 
     public GlobalUser getUser(int id) {
-        RequestSpecification spec = given()
-                .baseUri("http://localhost:8080/api");
-        
-        User user = given().spec(spec)
-                .contentType(ContentType.JSON)
-                .get("/users/" + id)
-                .then()
-                .extract().as(User.class);
 
-        List<Order> orderList = Arrays.stream(given().spec(spec)
-                .contentType(ContentType.JSON)
-                .get("/orders/by-user/" + id)
-                .then()
-                .extract().as(Order[].class)).toList();
+        RestClient restClient = RestClient.create("http://localhost:8080/api");
 
-        return new GlobalUser(user, orderList);
+        User user = restClient
+                .get()
+                .uri("/users/" + id)
+                .retrieve()
+                .body(User.class);
+
+        Order[] orderList = restClient
+                .get()
+                .uri("/orders/by-user/" + id)
+                .retrieve().body(Order[].class);
+
+        return new GlobalUser(user, Arrays.stream(orderList).toList());
     }
 }
